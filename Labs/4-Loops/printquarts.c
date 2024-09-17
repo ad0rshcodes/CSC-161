8e3abd1d6407fc0b5a88e130d54259309f5c7fd5
ad0rshcodes <adarsharma.me@gmail.com>
2024-09-17 18:56:56 -0500

    lab report week 2

--- Labs/4-Loops/printquarts.c
+++ Labs/4-Loops/printquarts.c
@@ -1,20 +1,30 @@
+/*
+ * lab: Loops
+ *
+ * \authors: Adarsh Sharma and Michael Getsos
+ * A. 1-2
+ */
+
 #include <stdio.h>
 
 int main()
 {
+
+  // Using For Loop
   printf("Table of quarts and liter equivalents\n");
   printf("Quarts         Liters\n");
   for (int i = 1; i <= 12; i++)
   {
-    printf("%d             %f\n", i, (0.9463 * i));
+    printf("  %d            %.4f\n", i, (0.9463 * i));
   }
 
+  // Using While Loop
   printf("Table of quarts and liter equivalents\n");
   printf("Quarts         Liters\n");
   int i = 1;
   while (i < 13)
   {
-    printf("%d             %f\n", i, (0.9463 * i));
+    printf("  %d            %.4f\n", i, (0.9463 * i));
     i++;
   }
 

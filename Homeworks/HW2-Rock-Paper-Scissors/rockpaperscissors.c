6c9f849cad23d718e89055ded07cf68e132395ae
ad0rshcodes <adarsharma.me@gmail.com>
2024-09-17 17:05:18 -0500

    Complete HW2

--- Homeworks/HW2-Rock-Paper-Scissors/rockpaperscissors.c
+++ Homeworks/HW2-Rock-Paper-Scissors/rockpaperscissors.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main()
 {
+    srand((unsigned)time(NULL));
+
     int rounds = 0;
 
     while (rounds <= 0)
@@ -32,19 +36,102 @@ int main()
 
         while (userResponse != 0 && userResponse != 1 && userResponse != 2)
         {
-            printf("Make your decision for the next round. \n 0: Rock \n 1: Paper \n 2: Scissors \n");
+            printf("Make your decision for the next round. \n 0: Rock \n 1: Paper \n 2: Scissors \n \n");
 
             printf("Your choice for this round: ");
             scanf("%d", &userResponse);
 
             if (userResponse != 0 && userResponse != 1 && userResponse != 2)
             {
-                printf("Your input value is invalid. Your input needs to one of the following numbers: 0, 1, 2. Anything apart from these will be an error!! \n Now try again. \n");
+                printf("\n\nYour input value is invalid. Your input needs to one of the following numbers: 0, 1, 2. Anything apart from these will be an error!! \n Now try again. \n \n");
             }
         }
 
-        // Rest of the code that will generate random number and compare the input with the random number.
+        // Generating random number between 0 to 2.
+        computerResponse = rand() % 3;
+
+        // test cases for each round
+        /*
+            0: rock
+            1: paper
+            2: scissors
+        */
+
+        if (userResponse == 0 && computerResponse == 0)
+        {
+            printf("Tie!! Both players choose rock.");
+            userWins++;
+            computerWins++;
+        }
+        else if (userResponse == 1 && computerResponse == 1)
+        {
+            printf("Tie!! Both players choose paper.");
+            userWins++;
+            computerWins++;
+        }
+        else if (userResponse == 2 && computerResponse == 2)
+        {
+            printf("Tie!! Both players choose scissors.");
+            userWins++;
+            computerWins++;
+        }
+        else if (userResponse == 0 && computerResponse == 1)
+        {
+            printf("Other player won!! You chose Rock and the other player chose Paper.");
+            computerWins++;
+        }
+        else if (userResponse == 0 && computerResponse == 2)
+        {
+            printf("You won!! You chose Rock and the other player chose Scissors.");
+            userWins++;
+        }
+        else if (userResponse == 1 && computerResponse == 0)
+        {
+            printf("You won!! You chose Paper and the other player chose Rock.");
+            userWins++;
+        }
+        else if (userResponse == 1 && computerResponse == 2)
+        {
+            printf("Other player won!! You chose Paper and the other player chose Scissors.");
+            computerWins++;
+        }
+        else if (userResponse == 2 && computerResponse == 0)
+        {
+            printf("Other player won!! You chose Scissors and the other player chose Rock.");
+            computerWins++;
+        }
+        else if (userResponse == 2 && computerResponse == 1)
+        {
+            printf("You won!! You chose Scissors and the other player chose Paper.");
+            userWins++;
+        }
+        else
+        {
+            printf("Something is not working as intended!!");
+        }
     }
+
+    printf("\n\nAll the rounds are completed!!\n\n");
+
+    if (userWins > computerWins)
+    {
+        printf("You WON!!");
+    }
+    else if (computerWins > userWins)
+    {
+        printf("You LOST!!");
+    }
+    else
+    {
+        printf("It's a TIE!!");
+    }
+
+    printf("\n Final Scores:\n");
+
+    printf("Your Wins: %d \n", userWins);
+    printf("Other player Wins: %d \n \n", computerWins);
+
+    printf("Thanks for Playing :)\n");
 
     return 0;
 }
